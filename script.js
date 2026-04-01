c50ad59ae31841b4d20bc64b6a0d02dd08df761d
MrNeoCo <neocoecomdev@gmail.com>
2026-04-01 16:05:21 +0530

    enhanced UI and UX with the search result and new cold color palette and all with fixed responsiveness

--- script.js
+++ script.js
@@ -13,6 +13,7 @@ search: Object.values(d).join(" ").toLowerCase()
 
 const search = document.getElementById("search");
 const results = document.getElementById("results");
+const resultCount = document.getElementById("result-count");
 const hint = document.getElementById("hint");
 
 search.addEventListener("input", function(){
@@ -23,6 +24,7 @@ results.innerHTML = "";
 
 if(query === ""){
 hint.style.display = "block";
+resultCount.textContent = "";
 return;
 }
 
@@ -31,18 +33,22 @@ hint.style.display = "none";
 let matches = devices
 .filter(d => d.search.includes(query))
 .slice(0,50);
+resultCount.textContent = matches.length + " results found";
 
 matches.forEach(d => {
 
 let card = document.createElement("div");
 card.className = "card";
 
-card.innerHTML = `<b>${d["Name"] || ""}</b><br>
-IP: ${d["IP"] || ""}<br>
-S/N: ${d["S/N"] || ""}<br>
-Last Sync: ${d["Last sync"] || ""}<br>
-Device ID: ${d["Device ID"] || ""}<br>
-System: ${d["System"] || ""}`;
+card.innerHTML = `
+<div class="card-title">${d["Name"] || ""}</div>
+
+<div class="device-row"><span>IP:</span>${d["IP"] || ""}</div>
+<div class="device-row"><span>S/N:</span>${d["S/N"] || ""}</div>
+<div class="device-row"><span>Last Sync:</span>${d["Last sync"] || ""}</div>
+<div class="device-row"><span>Device ID:</span>${d["Device ID"] || ""}</div>
+<div class="device-row"><span>System:</span>${d["System"] || ""}</div>
+`;
 
 results.appendChild(card);
 
